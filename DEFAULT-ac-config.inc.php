5973efb30ff6016ed999dbf3cb54cccc6ad348ce
cbolson <chris@cbolson.com>
2023-01-06 14:11:18 +0100

    add DEFAULT-ac-config.inc.php file This file has no private config details to avoid uploading db access data

---
+++ DEFAULT-ac-config.inc.php
@@ -0,0 +1,12 @@
+<?php
+//	 database settings
+define("AC_DB_HOST",		"");
+define("AC_DB_NAME",		"");
+define("AC_DB_USER",		"");
+define("AC_DB_PASS",		"");
+define("AC_DB_PREFIX",		"");
+//	do not alter these lines
+define("AC_ROOT"			, dirname(__FILE__). "/");
+define("AC_INCLUDES_ROOT"	, AC_ROOT."ac-includes/");
+define("CAL_VERSION"	    , "v3.04.02");
+?>
